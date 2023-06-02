e15b0f40edb00f1b52a43ed013ed70083f910396
Natsuki Okada <coconana.9042@gmail.com>
2023-06-02 22:23:23 +0900

    WIP adding validation / trying with hidden elements

--- script.js
+++ script.js
@@ -1,23 +1,66 @@
-document.getElementById("myform").addEventListener("submit", function(event) {
+// フォームの送信イベントを監視し、バリデーションを実行する
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    // デフォルトのフォーム送信をキャンセル
     event.preventDefault();
-
-    const firstNameInput = document.getElementById("firstName");
-    const lastNameInput = document.getElementById("lastName");
-    const emailInput = document.getElementById("email");
-    const passwordInput = document.getElementById("password");
-
-    const firstName = firstNameInput.value.trim();
-    const lastName = lastNameInput.value.trim();
-    const email = emailInput.value.trim();
-    const password = passwordInput.value.trim();
-
-    const errorMessages = [];
-
-    if (firstName === "") {
-        errorMessages.push("First Name cannot be empty");
-        firstNameInput.classList.add("error");
-        
+  
+    // 各入力フィールドの値を取得
+    var firstNameInput = document.getElementById('firstName');
+    var lastNameInput = document.getElementById('lastName');
+    var emailInput = document.getElementById('email');
+    var passwordInput = document.getElementById('password');
+  
+    // 各エラーメッセージの要素を取得
+    var firstNameError = document.getElementById('firstNameError');
+    var lastNameError = document.getElementById('lastNameError');
+    var emailError = document.getElementById('emailError');
+    var passwordError = document.getElementById('passwordError');
+  
+    // エラーメッセージをリセット（非表示にする）
+    firstNameError.style.display = 'none';
+    lastNameError.style.display = 'none';
+    emailError.style.display = 'none';
+    passwordError.style.display = 'none';
+  
+    // フォームのバリデーションを実行
+    var isValid = true;
+  
+    if (firstNameInput.value.trim() === '') {
+      firstNameError.innerText = 'First Name cannot be empty';
+      firstNameError.style.display = 'block';
+      isValid = false;
     }
-
-
-}
+  
+    if (lastNameInput.value.trim() === '') {
+      lastNameError.innerText = 'Last Name cannot be empty';
+      lastNameError.style.display = 'block';
+      isValid = false;
+    }
+  
+    if (emailInput.value.trim() === '') {
+      emailError.innerText = 'Email Address cannot be empty';
+      emailError.style.display = 'block';
+      isValid = false;
+    } else if (!isValidEmail(emailInput.value.trim())) {
+      emailError.innerText = 'Looks like this is not an email';
+      emailError.style.display = 'block';
+      isValid = false;
+    }
+  
+    if (passwordInput.value.trim() === '') {
+      passwordError.innerText = 'Password cannot be empty';
+      passwordError.style.display = 'block';
+      isValid = false;
+    }
+  
+    // バリデーションが成功した場合にのみフォームを送信
+    if (isValid) {
+      // フォームの送信処理を実行
+      this.submit();
+    }
+  });
+  
+  // 正規表現を使用してメールアドレスの形式をチェックする関数
+  function isValidEmail(email) {
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  }
