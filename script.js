4e89cff9940c03df32cdc34fbc7aeb62580522fb
Natsuki Okada <coconana.9042@gmail.com>
2023-06-11 21:54:43 +0900

    Finishing the inputs styling

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ myButton.addEventListener('click', runValidation);
 function runValidation(){
 
     // 各入力フィールドの値を取得
-    var firstNameInput = document.getElementById('firstName');
-    var lastNameInput = document.getElementById('lastName');
-    var emailInput = document.getElementById('email');
-    var passwordInput = document.getElementById('password');
+    let firstNameInput = document.getElementById('firstName');
+    let lastNameInput = document.getElementById('lastName');
+    let emailInput = document.getElementById('email');
+    let passwordInput = document.getElementById('password');
   
     // 各エラーメッセージの要素を取得
-    var firstNameError = document.getElementById('firstNameError');
-    var lastNameError = document.getElementById('lastNameError');
-    var emailError = document.getElementById('emailError');
-    var passwordError = document.getElementById('passwordError');
+    let firstNameError = document.getElementById('firstNameError');
+    let lastNameError = document.getElementById('lastNameError');
+    let emailError = document.getElementById('emailError');
+    let passwordError = document.getElementById('passwordError');
   
     // エラーメッセージをリセット（非表示にする）
     firstNameError.style.display = 'none';
@@ -21,33 +21,42 @@ function runValidation(){
     emailError.style.display = 'none';
     passwordError.style.display = 'none';
 
+    firstNameInput.classList.remove('error');
+    lastNameInput.classList.remove('error');
+    emailInput.classList.remove('error');
+    passwordInput.classList.remove('error');
+
     if (firstNameInput.value.trim() === '') {
       firstNameError.innerText = 'First Name cannot be empty';
       firstNameError.style.display = 'block';
-      
+      firstNameInput.classList.add('error');
     }
-  
+      
     if (lastNameInput.value.trim() === '') {
       lastNameError.innerText = 'Last Name cannot be empty';
       lastNameError.style.display = 'block';
+      lastNameInput.classList.add('error');
     }
   
     if (emailInput.value.trim() === '') {
       emailError.innerText = 'Email Address cannot be empty';
       emailError.style.display = 'block';
+      emailInput.classList.add('error');
     } else if (!isValidEmail(emailInput.value.trim())) {
       emailError.innerText = 'Looks like this is not an email';
       emailError.style.display = 'block';
+      emailInput.classList.add('error');
     }
   
     if (passwordInput.value.trim() === '') {
       passwordError.innerText = 'Password cannot be empty';
       passwordError.style.display = 'block';
+      passwordInput.classList.add('error');
     }
    
   // 正規表現を使用してメールアドレスの形式をチェックする関数
   function isValidEmail(email) {
-    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailPattern.test(email);
   }
 }
